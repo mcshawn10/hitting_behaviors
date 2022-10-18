6e84cb56e1759de284c7ee9b3fdeef56480b26f9
mcshawn10 <shawnmcneill10@gmail.com>
2022-10-17 22:38:17 -0500

    new mod

---
+++ rescale_frame.py
@@ -0,0 +1,10 @@
+import cv2
+import mediapipe as mp
+import numpy as np
+
+
+def rescale_frame(frame, percent):
+    width = int(frame.shape[1] * percent/100)
+    height = int(frame.shape[0] * percent/100)
+    dim = (width, height)
+    return cv2.resize(frame, dim, interpolation=cv2.INTER_AREA)
