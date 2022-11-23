a2f8fff84383d89ab3f946857ffb8ca030426311
mcshawn10 <shawnmcneill10@gmail.com>
2022-11-23 12:03:32 -0600

    I believe it's working

--- monitor.py
+++ monitor.py
@@ -1,22 +1,41 @@
 import numpy as np
 import pandas as pd
-#import tensorflow as tf 
+import tensorflow as tf 
 import cv2 
 import os
 import time
 
 
-print(cv2.__version__)
+def display_prediction(frame, categories, i, font):
+    RED, GREEN = (0,0,255),(0,255,0)
+    if i == 1:
+        cv2.putText(frame, 
+                    categories[i], 
+                    (5,25),
+                    font, 1,
+                    GREEN,
+                    1,
+                    cv2.LINE_4)
+    else:
+        cv2.putText(frame, 
+                    categories[i], 
+                    (5,25),
+                    font, 1,
+                    RED,
+                    1,
+                    cv2.LINE_4)
 
 def monitor(sample):
     assert os.path.exists(sample)
-    #EffNet = tf.keras.model.load_model("./EffNet_1")
+    EffNet = tf.keras.models.load_model("./EffNet_1")
     font = cv2.FONT_HERSHEY_SIMPLEX
-
+    categories = ["HITTING", "none"]
     cap = cv2.VideoCapture(sample)
     
     if not cap.isOpened():
         raise IOError("cannot open webcam")
+
+    hitting_count = 0
     
     while cap.isOpened():
 
@@ -24,32 +43,37 @@ def monitor(sample):
         
         #assert success
 
-        # fps = cap.get(cv2.CAP_PROP_FPS)      # OpenCV v2.x used "CV_CAP_PROP_FPS"
-        # print(fps)
-        # assert fps >0
+        
         frame_count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
-        # duration = frame_count/fps #in seconds
+        
         current_frame = cap.get(cv2.CAP_PROP_POS_FRAMES)
-        #interval = int(fps*5)
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
         frame = cv2.resize(frame, (224, 224), interpolation=cv2.INTER_AREA)
 
+        input_arr = frame.astype("float32") / 255
+
+        prediction = EffNet.predict(np.expand_dims(input_arr, axis=0))
+        #cv2.imshow('original video', frame)
+        #print(prediction)
+
+        i = 0
+
+        if prediction[0][0] <= 0.5:
+            i = 0
+        else: i = 1
+
+        #if current_frame % 20 == 0:
+
+        # cv2.putText(frame, 
+        #             categories[i], 
+        #             (5,25),
+        #             font, 1,
+        #             (0,0,255),
+        #             1,
+        #             cv2.LINE_4)
+        display_prediction(frame, categories, i,font)
         cv2.imshow('original video', frame)
-
-        '''if current frame == interval, make a prediction on the video'''
-        
-
-        if current_frame % 20 == 0:
-
-            cv2.putText(frame, 
-                        "Prediction", 
-                        (25,50),
-                        font, 2,
-                        (0,0,255),
-                        1,
-                        cv2.LINE_4)
-
-            cv2.imshow('original video', frame)
-        
+    
 
         
 
@@ -57,7 +81,7 @@ def monitor(sample):
 
 
         if cv2.waitKey(5) & 0xFF == 27:
-            print("broked")
+            
             break
         
     cv2.waitKey(0)
